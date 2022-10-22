cc66e701b7398fb51daa55528b9cc100bf89055c
jendlrs <jensennics.dlrs@gmail.com>
2022-10-22 16:16:59 +0800

    The code is final. I just added text color to the asterisks.

--- AsteriskNaming.py
+++ AsteriskNaming.py
@@ -21,21 +21,21 @@ printN = [[" " for i in range(5)] for j in range(5)]
 for row in range(5):
    for col in range(5):
       if (col == 0 and row == 3) or (row == 4 and (col != 0 and col != 4)) or (col == 4 and row != 4):
-         printJ[row][col] = "*"
+         printJ[row][col] = "\033[95m*\033[0m"         #Change color of asterisk to pink
 
 #LETTER E
 for row in range(5):
    for col in range(5):
-      if (row == 0 or row == 2 or row == 4) or (col == 0 or row == 1 and row == 3):
-         printE[row][col] = "*"
+      if (col == 0 or (row == 0 or row == 4)) or ((row ==2 and col !=5 and col !=4)):
+         printE[row][col] = "\033[96m*\033[0m"         #Change color of asterisk to light blue
 
 #LETTER N
 for row in range (5):
    for col in range (5):
       if col == 0 or col == 4 or (row == col and (col>0 and col <4)):
-         printN[row][col] = "*"
+         printN[row][col] = "\033[95m*\033[0m"         #Change color of asterisk to pink
 
-#DISPLAYING WHOLE NICKNAME (JEN)
+#DISPLAYING THE WHOLE NICKNAME (JEN)
 for i in range(5):
    for j in range(5):
       print(printJ[i][j], end = " ")
@@ -46,4 +46,3 @@ for i in range(5):
    for j in range(5):
       print(printN[i][j], end = " ")
    print()
-
